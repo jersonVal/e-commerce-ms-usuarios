783e482a49a7674c56db9b4d5660a87acad24969
jersonval96 <jersonDevelopment@gmail.com>
2021-10-02 16:18:50 -0500

    recognize users

--- src/controllers/usuario.controller.ts
+++ src/controllers/usuario.controller.ts
@@ -18,7 +18,7 @@ import {
   requestBody,
   response,
 } from '@loopback/rest';
-import {Usuario} from '../models';
+import {Credenciales, Usuario} from '../models';
 import {UsuarioRepository} from '../repositories';
 import { AdministradorClavesService } from '../services';
 
@@ -160,5 +160,34 @@ export class UsuarioController {
   })
   async deleteById(@param.path.string('id') id: string): Promise<void> {
     await this.usuarioRepository.deleteById(id);
+  }
+
+  //METODOS ADICIONALES
+
+  @post('/identificar-usuario')
+  @response(200, {
+    description: 'Identificacion de usuarios',
+    content: {'application/json': {schema: getModelSchemaRef(Credenciales)}},
+  })
+  async identificarUsuario(
+    @requestBody({
+      content: {
+        'application/json': {
+          schema: getModelSchemaRef(Credenciales, {
+            title: 'Identificar usuario'
+          }),
+        },
+      },
+    })
+    credenciales: Credenciales,
+  ): Promise<Usuario | null> {
+    let usuario = await this.usuarioRepository.findOne({
+      where:{
+        correo: credenciales.usuario,
+        clave: credenciales.clave
+      }
+    })
+    console.log(usuario)
+    return usuario;
   }
 }
